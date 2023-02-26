9d7760cdff7b44740ec24bfa416f47d484925feb
Mavrayev <mavrayev_a@mail.ru>
2023-02-26 07:41:25 +0500

    feat: setupt bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   server: {
     port: 8080,
     open: true,
-
   }
 })
