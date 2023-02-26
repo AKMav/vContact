769a3da49ecc765b565dbc7dbf0a74ffde3de958
Mavrayev <mavrayev_a@mail.ru>
2023-02-26 08:11:18 +0500

    feat: setup vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import './assets/styles/main.scss'
-import App from './App.vue'
-import * as bootstrap from 'bootstrap'
+/* eslint-disable no-unused-vars */
+import { createApp } from "vue";
+import router from "./router";
+import "./assets/styles/main.scss";
+import App from "./App.vue";
+import * as bootstrap from "bootstrap";
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)  
+.mount("#app");
